8bedea4e459ec34141ef2ffbce9ff55a1e34f65d
ryoogata <ryo.ogata@gmail.com>
2013-11-28 23:02:57 +0900

    設定ファイル Download を追記

--- recipes/init_setup_splunk.rb
+++ recipes/init_setup_splunk.rb
@@ -1,3 +1,13 @@
+# splunk の初期化を Selenium で自動化する為のコードを設置
+template "/tmp/init_setup_splunk.rb" do
+	source "init_setup_splunk.rb"
+	variables(
+		:loginpassword => node["splunk"]["_LOGINPASSWORD"]
+	)
+end
+
+
+# Splunk の初期設定を実施
 execute "SetupSplunk" do
 	command "/usr/local/bin/rspec /tmp/init_setup_splunk.rb"
 end
