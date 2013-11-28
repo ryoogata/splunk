6327d8d699ac3b7fe3edc85d97bb01ded52e9fc9
ryoogata <ryo.ogata@gmail.com>
2013-11-28 23:00:38 +0900

    Source の Download URL を attribute で指定するように変更

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,4 @@
-default["splunk"]["_LOGINPASSWORD"] = "password"
+default["splunk"]["_LOGINPASSWORD"] = ""
+default["splunk"]["_DOWNLOAD_URL"]["SPLUNK_RPM"] = ""
+default["splunk"]["_DOWNLOAD_URL"]["SPLUNK_DEB"] = ""
+default["splunk"]["_DOWNLOAD_URL"]["SPLUNKAPPFORAWS"] = ""
