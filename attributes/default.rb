33946f6aff0ecf326ea9f60ed5959c11b183f0d5
ryoogata <ryo.ogata@gmail.com>
2013-11-29 23:12:18 +0900

    Splunk App for AWS の設定関連の attribute を追加

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,10 @@ default["splunk"]["_LOGINPASSWORD"] = ""
 default["splunk"]["_DOWNLOAD_URL"]["SPLUNK_RPM"] = ""
 default["splunk"]["_DOWNLOAD_URL"]["SPLUNK_DEB"] = ""
 default["splunk"]["_DOWNLOAD_URL"]["SPLUNKAPPFORAWS"] = ""
+
+default["splunkappforaws"]["_NAME"] = "MyCloudTrail"
+
+default["aws"]["_ACCESS_KEY"] = ""
+default["aws"]["_SECRET_KEY"] = ""
+default["aws"]["sqs"]["_SQS_QUEUE"] = ""
+default["aws"]["sqs"]["_SQS_QUEUE_REGION"] = "us-east-1"
