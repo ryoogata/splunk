a790a4ca3d61b8a6939da1cee8fd4f9af647deec
ryoogata <ryo.ogata@gmail.com>
2013-12-28 23:07:58 +0900

    Version を変更

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures splunk'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.2.0'
 
 supports "centos"
 supports "ubuntu"
