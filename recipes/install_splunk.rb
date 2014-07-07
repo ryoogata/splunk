21306cf2ef4756e20d3394e020b1a8296570edbf
ryoogata <ryo.ogata@gmail.com>
2014-07-07 16:56:21 +0900

    インデントの調整

--- recipes/install_splunk.rb
+++ recipes/install_splunk.rb
@@ -1,13 +1,13 @@
 # 必要なパッケージの設置
 case node['platform']
 when "centos"
-	remote_file "/tmp/splunk.rpm" do
-		source "#{node['splunk']['_DOWNLOAD_URL']['SPLUNK_RPM']}"
-	end
+  remote_file "/tmp/splunk.rpm" do
+    source "#{node['splunk']['_DOWNLOAD_URL']['SPLUNK_RPM']}"
+  end
 when "ubuntu"
-	remote_file "/tmp/splunk.deb" do
-		source "#{node['splunk']['_DOWNLOAD_URL']['SPLUNK_DEB']}"
-	end
+  remote_file "/tmp/splunk.deb" do
+    source "#{node['splunk']['_DOWNLOAD_URL']['SPLUNK_DEB']}"
+  end
 end
 
 
@@ -20,33 +20,30 @@ end
 # 必要なパッケージのインストール
 case node['platform']
 when "ubuntu"
-	dpkg_package "splunk" do
-		action :install
-		source "/tmp/splunk.deb"
-	end
+  dpkg_package "splunk" do
+    action :install
+    source "/tmp/splunk.deb"
+  end
 when "centos"
-	package "splunk" do
-		action :install
-		source "/tmp/splunk.rpm"
-		provider Chef::Provider::Package::Rpm
-	end
+  package "splunk" do
+    action :install
+    source "/tmp/splunk.rpm"
+    provider Chef::Provider::Package::Rpm
+  end
 end
-
 
 # license-eula.txt の削除
 file "/opt/splunk/license-eula.txt" do
-	action :delete
+  action :delete
 end
-
 
 # splunk の初期化
 execute "boot-start" do
-        command "/opt/splunk/bin/splunk enable boot-start --answer-yes"
+  command "/opt/splunk/bin/splunk enable boot-start --answer-yes"
 end
-
 
 # splunk の起動
 service "splunk" do
-	supports :status => true, :restart => true, :reload => true
-	action [ :enable, :start ]
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
 end
